efc0b70d9a64d4d10349caec33f93e73c963874f
sandraarambulo <arambulosandra5@gmail.com>
2024-07-05 08:32:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var product1 = document.getElementById("product1")
+            var product1 = document.getElementById("product1")
             var qty1 = document.getElementById("qty1")
             var price1 = document.getElementById("price1")
 
             var product2 = document.getElementById("product2")
             var qty2 = document.getElementById("qty2")
             var price2 = document.getElementById("price2")
+
+            var product3 = document.getElementById("product3")
+            var qty3 = document.getElementById("qty3")
+            var price3 = document.getElementById("price3")
+
+            var product4 = document.getElementById("product4")
+            var qty4 = document.getElementById("qty4")
+            var price4 = document.getElementById("price4")
+
+            var product5 = document.getElementById("product5")
+            var qty5 = document.getElementById("qty5")
+            var price5 = document.getElementById("price5")
 
             var carts = document.getElementById("carts")
             var total = document.getElementById("total")
